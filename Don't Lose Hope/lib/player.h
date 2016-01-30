5b704d70f8868efe2a92c0c6ab47c422616f3313
Leonardo <leonardostsouza@gmail.com>
2016-01-30 16:41:11 -0200

    create player.h

---
+++ Don't Lose Hope/lib/player.h
@@ -0,0 +1,22 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+#ifndef PLAYER_MAX_LIFES
+#define PLAYER_MAX_LIFES 3
+
+class Player
+{
+public:
+	Player(){
+		current_lifes = PLAYER_MAX_LIFES;
+	}
+	~Player();
+
+	void setLifes(unsigned int lifeQtt){current_lifes = lifeQtt;}
+	unsigned int getLifes(){return current_lifes;}
+
+private:
+	unsigned int current_lifes;
+};
+
+#endif
+#endif
