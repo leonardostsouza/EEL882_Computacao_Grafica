f9fa442bff85486746daa18634e76f022ec22962
Raphael Sathler <sathler93@gmail.com>
2016-02-10 16:36:45 -0200

    Criei um Game, Menu Options, Consertei o MakeFile, Ajeitei o Screen e o Main, além da resolução que tava bugada. Tem que verificar a música - minha VM não toca.

--- Don't Lose Hope/main.cpp
+++ Don't Lose Hope/main.cpp
@@ -1,18 +1,17 @@
-#include "lib/Screen.h"
+#include "lib/screen.h"
+#include "lib/utilities.h"
+#include "lib/game.h"
 #include <string.h>
 
-#ifdef DEBUG
-#include <stdio.h>
-#endif
-
-using namespace std;
+//using namespace std;
 
 int main(int argc, char *argv[])
 {
+    GAMESTATE state = MAINMENU;
 	bool fs = false;
     // check for execution flags
     #ifdef DEBUG
-    printf("DEBUG === Entering flag parse loop\n");
+    std::cout << "DEBUG === Entering flag parse loop" << std::endl;
     #endif
     for (int iterator = 1; iterator < argc; iterator++)
     {
@@ -20,12 +19,67 @@ int main(int argc, char *argv[])
         {
 			fs = true;
             #ifdef DEBUG
-            printf("DEBUG === fullscreen set to %d\n", fs);
+            std::cout << "DEBUG === fullscreen set to " << fs << std::endl;
             #endif
 		}
     }
 
-	Screen screen(fs);
+    sf::VideoMode vmode;
+    sf::RenderWindow* window;
+    //set VideoMode
+
+    if (!fs)
+    {
+        vmode.width = DEFAULT_WINDOW_WIDTH;
+        vmode.height = DEFAULT_WINDOW_HEIGHT;
+        vmode.bitsPerPixel = sf::VideoMode::getDesktopMode().bitsPerPixel;
+    }
+    else
+    {
+        vmode = sf::VideoMode::getFullscreenModes()[0];
+    }
+
+    // Create the main window
+    window = new sf::RenderWindow(sf::VideoMode(vmode.width, vmode.height, vmode.bitsPerPixel), "Don't Lose Hope");
+
+	Screen screen(fs,window,vmode);
+    Game game(fs,window,vmode);
+
+    while ((*window).isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while ((*window).pollEvent(event))
+        {
+            switch (state){
+                case MAINMENU:
+                    state = screen.eventHandler(state);
+                    std::cout << "Main Menu" << std::endl;
+                    break;
+
+                case OPTIONSMENU:
+                    state = screen.eventHandler(state);
+                    std::cout << "Options" << std::endl;
+                    break;
+
+                case PLAYING:
+                    state = game.eventHandler(state);
+                    std::cout << "Playing" << std::endl;
+                    break;                
+
+                case CLOSE:
+                    std::cout << "Close" << std::endl;
+                    (*window).close();
+                    break;
+
+                default:
+                    state = MAINMENU;
+            }
+            // close window event
+            if (event.type == sf::Event::Closed)
+                (*window).close();
+        }
+    }
 
    return EXIT_SUCCESS;
 }
