3ee175ada5db1fac30496f0eda42adcb0ca899f3
Raphael Sathler <sathler93@gmail.com>
2016-02-18 15:24:16 -0200

    Solved fullscreen issue and resizement

--- Don't Lose Hope/main.cpp
+++ Don't Lose Hope/main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     GAMESTATE state = MAINMENU, prevState = state;
-	bool fs = false;
+    bool fs = false;
     // check for execution flags
     #ifdef DEBUG
     std::cout << "DEBUG === Entering flag parse loop" << std::endl;
@@ -17,69 +17,81 @@ int main(int argc, char *argv[])
     {
         if (strcmp(argv[1],"-f") == 0)
         {
-			fs = true;
+         fs = true;
             #ifdef DEBUG
-            std::cout << "DEBUG === fullscreen set to " << fs << std::endl;
+         std::cout << "DEBUG === fullscreen set to " << fs << std::endl;
             #endif
-		}
-    }
+     }
+ }
 
-    sf::VideoMode vmode;
-    sf::RenderWindow* App;
+ sf::VideoMode vmode;
+ sf::RenderWindow* App;
 
     //set VideoMode
-    if (!fs)
-    {
-        vmode.width = DEFAULT_WINDOW_WIDTH;
-        vmode.height = DEFAULT_WINDOW_HEIGHT;
-        vmode.bitsPerPixel = sf::VideoMode::getDesktopMode().bitsPerPixel;
-    }
-    else
-    {
-        vmode = sf::VideoMode::getFullscreenModes()[0];
-    }
+ if (!fs)
+ {
+    vmode.width = DEFAULT_WINDOW_WIDTH;
+    vmode.height = DEFAULT_WINDOW_HEIGHT;
+    vmode.bitsPerPixel = sf::VideoMode::getDesktopMode().bitsPerPixel;
+}
+else
+{
+    vmode = sf::VideoMode::getFullscreenModes()[0];
+}
 
     // Create the main window
-    App = new sf::RenderWindow(sf::VideoMode(vmode.width, vmode.height, vmode.bitsPerPixel), "Don't Lose Hope");
-    App->setFramerateLimit(60);
-    App->setVerticalSyncEnabled(true);
-	Screen menu(fs,App,vmode);
-    Game game(fs,App,vmode);
+App = new sf::RenderWindow(sf::VideoMode(vmode.width, vmode.height, vmode.bitsPerPixel), "Don't Lose Hope");
+App->setFramerateLimit(60);
+App->setVerticalSyncEnabled(true);
+Screen menu(fs,App,vmode);
+Game game(fs,App,vmode);
 
     // check all the window's events that were triggered since the last iteration of the loop
         while (App->isOpen()) //.pollEvent(event))
-        {
+{
 
-            sf::Event event;
-            switch (state){
-                case MAINMENU:
-                    state = menu.eventHandler(state);
-                    break;
+    sf::Event event;
+    switch (state){
+        case MAINMENU:
+        state = menu.eventHandler(state);
+        break;
 
-                case OPTIONSMENU:
-                    state = menu.eventHandler(state);
-                    break;
+        case OPTIONSMENU:
+        state = menu.eventHandler(state);
+        break;
 
-                case PLAYING:
-                    state = game.eventHandler(menu.isFullscreen(), menu.isSoundEnabled());
-                    break;                
+        case PLAYING:
+        state = game.eventHandler(menu.isFullscreen(), menu.isSoundEnabled());
+        break;                
 
-                case CLOSE:
-                    App->close();
-                    break;
+        case CLOSE:
+        App->close();
+        break;
 
-                default:
-                    state = MAINMENU;
-            }
-
-            prevState = state;
-            //close window event
-            App->pollEvent(event);
-            if (event.type == sf::Event::Closed)
-                App->close();
-            //if (event.type == sf::Event::Resized)
-                //App->SetView(view = sf::View(sf::FloatRect(0.f,0.f,static_cast<float>(App->.GetWidth()),static_cast<float>(App->.GetHeight))));
+        default:
+        state = MAINMENU;
     }
 
-   return EXIT_SUCCESS;
+    prevState = state;
+            //close window event
+    while(App->pollEvent(event)){
+        if (event.type == sf::Event::Closed)
+            App->close();
+        if (event.type == sf::Event::Resized){
+            if (!fs){
+                vmode = sf::VideoMode::getFullscreenModes()[0];
+            }else{
+                vmode.width = DEFAULT_WINDOW_WIDTH;
+                vmode.height = DEFAULT_WINDOW_HEIGHT;
+                vmode.bitsPerPixel = sf::VideoMode::getDesktopMode().bitsPerPixel;
+            }
+
+                //App->close();
+                //App->create(vmode, "Don't Lose Hope");
+        }
+    }
+                //App->SetView(view = sf::View(sf::FloatRect(0.f,0.f,static_cast<float>(App->.GetWidth()),static_cast<float>(App->.GetHeight))));
+}
+
+return EXIT_SUCCESS;
 }
